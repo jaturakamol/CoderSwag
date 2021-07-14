1ed4c768eff2ca538d443f22645dcbb2567a390a
jaturakamol <jatura_km@hotmail.com>
2021-07-14 21:12:57 +0700

    lesson-08

--- app/src/main/java/com/kamol/coderswag/Services/DataService.kt
+++ app/src/main/java/com/kamol/coderswag/Services/DataService.kt
@@ -24,10 +24,26 @@ object DataService {
         Product("Kamol Graphic Beanie", "$18", "hat1"),
         Product("Kamol Hat Black", "$19", "hat2"),
         Product("Kamol Hat White", "$20", "hat3"),
+        Product("Kamol Hat Black", "$21", "hat4"),
+        Product("Kamol Graphic Beanie", "$18", "hat1"),
+        Product("Kamol Hat Black", "$19", "hat2"),
+        Product("Kamol Hat White", "$20", "hat3"),
+        Product("Kamol Hat Black", "$21", "hat4"),
+        Product("Kamol Graphic Beanie", "$18", "hat1"),
+        Product("Kamol Hat Black", "$19", "hat2"),
+        Product("Kamol Hat White", "$20", "hat3"),
         Product("Kamol Hat Black", "$21", "hat4")
     )
 
     val hoodies = listOf(
+        Product("Kamol Hoodie Gray", "$25", "hoodie1"),
+        Product("Kamol Hoodie Red", "$26", "hoodie2"),
+        Product("Kamol Hoodie Gray", "$27", "hoodie3"),
+        Product("Kamol Hoodie Gray", "$28", "hoodie4"),
+        Product("Kamol Hoodie Gray", "$25", "hoodie1"),
+        Product("Kamol Hoodie Red", "$26", "hoodie2"),
+        Product("Kamol Hoodie Gray", "$27", "hoodie3"),
+        Product("Kamol Hoodie Gray", "$28", "hoodie4"),
         Product("Kamol Hoodie Gray", "$25", "hoodie1"),
         Product("Kamol Hoodie Red", "$26", "hoodie2"),
         Product("Kamol Hoodie Gray", "$27", "hoodie3"),
@@ -39,12 +55,22 @@ object DataService {
         Product("Kamol Badge Light Gray", "$32", "shirt2"),
         Product("Kamol Logo Shirt Red", "$33", "shirt3"),
         Product("Kamol Hustle", "$34", "shirt4"),
+        Product("Kickflip Studios", "$35", "shirt5"),
+        Product("Kamol Shirt Black", "$31", "shirt1"),
+        Product("Kamol Badge Light Gray", "$32", "shirt2"),
+        Product("Kamol Logo Shirt Red", "$33", "shirt3"),
+        Product("Kamol Hustle", "$34", "shirt4"),
+        Product("Kickflip Studios", "$35", "shirt5"),
+        Product("Kamol Shirt Black", "$31", "shirt1"),
+        Product("Kamol Badge Light Gray", "$32", "shirt2"),
+        Product("Kamol Logo Shirt Red", "$33", "shirt3"),
+        Product("Kamol Hustle", "$34", "shirt4"),
         Product("Kickflip Studios", "$35", "shirt5")
     )
 
     val digitalGood = listOf<Product>()
 
-    fun getProducts(category: String) : List<Product> {
+    fun getProducts(category: String?) : List<Product> {
 
         return when(category) {
             "SHIRTS" -> shirts
