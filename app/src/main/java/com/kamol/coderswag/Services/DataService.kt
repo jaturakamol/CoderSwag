b8572dc25bed20429aaa5db6c10e7273c3c577d3
jaturakamol <jatura_km@hotmail.com>
2021-07-14 19:36:42 +0700

    lesson-08

--- app/src/main/java/com/kamol/coderswag/Services/DataService.kt
+++ app/src/main/java/com/kamol/coderswag/Services/DataService.kt
@@ -21,25 +21,37 @@ object DataService {
     )
 
     val hats = listOf(
-        Product("Kamol Graphic Beanie", "$18", "hat01"),
-        Product("Kamol Hat Black", "$19", "hat02"),
-        Product("Kamol Hat White", "$20", "hat03"),
-        Product("Kamol Hat Black", "$21", "hat04")
+        Product("Kamol Graphic Beanie", "$18", "hat1"),
+        Product("Kamol Hat Black", "$19", "hat2"),
+        Product("Kamol Hat White", "$20", "hat3"),
+        Product("Kamol Hat Black", "$21", "hat4")
     )
 
     val hoodies = listOf(
-        Product("Kamol Hoodie Gray", "$25", "hoodie01"),
-        Product("Kamol Hoodie Red", "$26", "hoodie02"),
-        Product("Kamol Hoodie Gray", "$27", "hoodie03"),
-        Product("Kamol Hoodie Gray", "$28", "hoodie04")
+        Product("Kamol Hoodie Gray", "$25", "hoodie1"),
+        Product("Kamol Hoodie Red", "$26", "hoodie2"),
+        Product("Kamol Hoodie Gray", "$27", "hoodie3"),
+        Product("Kamol Hoodie Gray", "$28", "hoodie4")
     )
 
     val shirts = listOf(
-        Product("Kamol Shirt Black", "$31", "shirt01"),
-        Product("Kamol Badge Light Gray", "$32", "shirt02"),
-        Product("Kamol Logo Shirt Red", "$33", "shirt03"),
-        Product("Kamol Hustle", "$34", "shirt04"),
-        Product("Kickflip Studios", "$35", "shirt05")
+        Product("Kamol Shirt Black", "$31", "shirt1"),
+        Product("Kamol Badge Light Gray", "$32", "shirt2"),
+        Product("Kamol Logo Shirt Red", "$33", "shirt3"),
+        Product("Kamol Hustle", "$34", "shirt4"),
+        Product("Kickflip Studios", "$35", "shirt5")
     )
+
+    val digitalGood = listOf<Product>()
+
+    fun getProducts(category: String) : List<Product> {
+
+        return when(category) {
+            "SHIRTS" -> shirts
+            "HATS" -> hats
+            "HOODIES" -> hoodies
+            else -> digitalGood
+        }
+    }
 
 }
